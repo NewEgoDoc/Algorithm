6075f5d967be558f338672f9785655b955a8a662
DESKTOP-J3IEFM9\User <juhan211@naver.com>
2021-07-22 01:05:02 +0900

    Day4.2

--- boj/boj17298오큰수.java
+++ boj/boj17298오큰수.java
@@ -12,17 +12,30 @@ public class boj17298오큰수 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         Stack<Integer> stack = new Stack<>();
+        Stack<Integer> answer = new Stack<Integer>();
         int n = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        String[] str = s.split(" ");
-        int[] array = new int[n];
+        String[] s = br.readLine().split(" ");
+        
 
-        for(int i =0; i<n; i++){
-            array[i] = Integer.parseInt(str[i]);
+        for(int i = n-1; i>=0; i--){
+            int num = Integer.parseInt(s[i]);
+            while(!stack.isEmpty() && num >= stack.peek()) {
+            	stack.pop();
+            }
+            if(stack.isEmpty()) answer.push(-1);
+            else answer.push(stack.peek());
+            stack.push(num);
         }
 
-
-
+        for(int i=0; i<n; i++) {
+        	if(i == n-1) bw.write(answer.pop()+"\n");
+        	else bw.write(answer.pop()+" ");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
+    
 }
+
