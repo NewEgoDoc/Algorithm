b65deb1769b94d6fde955cac1d292a166d38a93d
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-07-28 17:05:31 +0900

    Day9.1 (8진수 2진수 문제: 정답인듯 하지만 시간을 고려하지 않아 시간초과로 틀림)

--- boj/boj1212팔진수2진수.java
+++ boj/boj1212팔진수2진수.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Stack;
 
 public class boj1212팔진수2진수 {
     public static void main(String[] args) throws Exception {
@@ -13,12 +14,24 @@ public class boj1212팔진수2진수 {
 
         String input = br.readLine();
 
+        Stack<Integer> stack = new Stack<>();
 
-        for(int i = 0; i < input.length(); i++){
+        for(int i = input.length()-1; i >= 0; i--){
             int num = input.charAt(i) -'0';
-
+            int count = 3;
+            while(count >0){
+                stack.push(num%2);
+                num /= 2;
+                count--;
+            }
         }
 
+        while(!stack.isEmpty()){
+            if(sb.toString().length() == 0 && stack.peek() == 0){
+                stack.pop();
+            }
+            sb.append(stack.pop());
+        }
 
         bw.write(sb.toString());
 
