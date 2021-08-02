6d63552c65029d94045777b4eac0ffeeaaf211d1
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-08-03 08:14:46 +0900

    Day14 카카오테스트 준비 실패율

--- kakao/Lv1실패율.java
+++ kakao/Lv1실패율.java
@@ -1,8 +1,20 @@
-package Algorithm.kakao;
+package algorithm.kakao;
 
 import java.util.*;
 
 public class Lv1실패율 {
+    static class  Fail {
+        int stage;
+        double failRate;
+
+        Fail(int stage, double failRate){
+            this.failRate = failRate;
+            this.stage = stage;
+        }
+    }
+
+
+
     public static int[] solution(int N, int[] stages) {
 //N 1~500 이하의 자연수
 //stages의 길이1~200,000 이하
@@ -10,36 +22,52 @@ public class Lv1실패율 {
 //스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 // 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열
         boolean []visit = new boolean[N+1];
-        int[] answer = new int[N];
         int[] fail = new int[N+1];
         for(int i = 0; i < stages.length; i++){
             if(stages[i]!=N+1){
-                fail[stages[i]-1]++;
+                fail[stages[i]]++;
             }
         }
 
-        for(int i = 0; i <= N; i++){
-            System.out.print(i+ ":"+fail[i] + " ");
-        }
-        System.out.println();
 
-        List<Double> failRate = new ArrayList<Double>();
+        List<Fail> failObj = new ArrayList<Fail>();
 
         int count = 0;
-        for(int i = 0; i< fail.length;i++) {
-            failRate.add(1.0*fail[i]/(stages.length-count));
+        for(int i = 1; i< fail.length;i++) {
+            failObj.add(new Fail(i ,1.0*fail[i]/(stages.length-count)));
             count += fail[i];
         }
-        System.out.println(failRate);
-        Collections.sort(failRate,Collections.reverseOrder());
-        System.out.println(failRate);
 
+        Comparator<Fail> com = new Comparator<Fail>() {
+            @Override
+            public int compare(Fail o1, Fail o2) {
+                if(o1.failRate > o2.failRate){
+                    return -1;
+                } else if(o1.failRate < o2.failRate){
+                    return 1;
+                } else {
+                    if(o1.stage > o2.stage){
+                        return 1;
+                    } else if(o1.stage < o2.stage){
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                }
+            }
+        };
 
+        Collections.sort(failObj,com);
+        int[] answer = new int[N];
+        for(int i = 0; i < N; i++){
+            answer[i] = failObj.get(i).stage;
+            System.out.println(answer[i]);
+        }
         return answer;
     }
 
     public static void main(String[] args){
-        int N=5;
+        int N = 5;
         int[] stages={2, 1, 2, 6, 2, 4, 3, 3};
         solution(N,stages);
     }
