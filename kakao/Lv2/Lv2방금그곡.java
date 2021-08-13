5c11f37c7c94cd02c0c529c82c75cdb4e0ecc22e
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-08-13 23:04:50 +0900

    Day21카카오 '방금 그곡' 다시 세심히 문제를 정리해서 알아보기 깔끔하게 코드 정리하였다 이전 코드도 정리해두었으니 비교해보려한다

--- kakao/Lv2/Lv2방금그곡.java
+++ kakao/Lv2/Lv2방금그곡.java
@@ -5,7 +5,56 @@ import java.util.Map;
 
 public class Lv2방금그곡 {
     public static String solution(String m, String[] musicinfos) {
+        String answer = "";
+        int maxRunningTime = 0;
+
+        m = m.replace("C#", "c")
+                .replace("D#", "d")
+                .replace("E#", "e")
+                .replace("F#", "f")
+                .replace("G#", "g")
+                .replace("A#", "a");;
+
+        for (String musicInfo : musicinfos) {
+            String[] info = musicInfo.split(",");
+
+            int hour = Integer.parseInt(info[1].split(":")[0]) - Integer.parseInt(info[0].split(":")[0]);
+            int minute = Integer.parseInt(info[1].split(":")[1]) - Integer.parseInt(info[0].split(":")[1]);
+            int runningTime = hour * 60 + minute;
+
+            String cords = info[3].replace("C#", "c")
+                    .replace("D#", "d")
+                    .replace("E#", "e")
+                    .replace("F#", "f")
+                    .replace("G#", "g")
+                    .replace("A#", "a");;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < runningTime; i++) {
+                stringBuilder.append(cords.charAt(i % cords.length()));
+            }
+
+            cords = stringBuilder.toString();
+
+            if (cords.contains(m)) {
+                if (maxRunningTime < cords.length()) {
+                    maxRunningTime = cords.length();
+                    answer = info[2];
+                }
+            }
+        }
+
+        if (answer.equals("")) {
+            return "(None)";
+        }
+
+        return answer;
+    }
+
+    /*public static String solution(String m, String[] musicinfos) {
         String a =m;
+        String answer ="";
+        int maxTime = 0;
         for(int i = 0; i < musicinfos.length; i++){
             String[] factors = musicinfos[i].split(",");
             StringBuilder sb = new StringBuilder();
@@ -18,18 +67,31 @@ public class Lv2방금그곡 {
                 //System.out.println(j+" : " +(j%(factors[3].length())));
                 sb.append(factors[3].charAt(j%(factors[3].length())));
             }
-            System.out.println(sb.toString());
-            if(sb.toString().contains(a)){
+
+            String str = sb.toString();
+            System.out.println(str);
+            if(str.contains(a)){
                 if(m.charAt(m.length()-1) == '#'){
-                    return factors[2];
+                    if (maxTime < time) {
+                        maxTime = time;
+                        answer = factors[2];
+                    }
                 } else {
-                    if(sb.toString().contains(m+"#")) continue;
-                    else return factors[2];
+                    if(str.contains(m+"#")) continue;
+                    else {
+                        if (maxTime < time) {
+                            maxTime = time;
+                            answer = factors[2];
+                        }
+                    }
                 }
             }
         }
-        return "(None)";
-    }
+
+        if(answer.equals("")) answer = "(None)";
+
+        return answer;
+    }*/
     public static void main(String[] args){
         String m = "ABC";
         String[] musicinfos ={"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"};
