c51e2febf76b3a7ae01544f33551f91579f2853e
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-08-18 20:41:50 +0900

    Day26 뉴스클러스팅 해당 합집합과 교집합의 관계를 수가 아닌 하나하나의 경우 모두 구해서 그 길이에 대해 연산 진행

--- kakao/Lv2/Lv2뉴스클러스팅.java
+++ kakao/Lv2/Lv2뉴스클러스팅.java
@@ -1,50 +1,63 @@
 package algorithm.kakao.Lv2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class Lv2뉴스클러스팅 {
     public static int solution(String str1, String str2) {
-        int answer = 0;
-        String newStr1 = str1.toLowerCase(Locale.ROOT).replaceAll("[^a-z]","");
-        String newStr2 = str2.toLowerCase(Locale.ROOT).replaceAll("[^a-z]","");;
+        String newStr1 = str1.toLowerCase(Locale.ROOT);//.replaceAll("[^a-z]","");
+        String newStr2 = str2.toLowerCase(Locale.ROOT);//.replaceAll("[^a-z]","");;
 
-        System.out.println(newStr1);
-        System.out.println(newStr2);
+
+        //System.out.println(newStr1);
+        //System.out.println(newStr2);
         int len1 = (newStr1.length()-1);
         int len2 = (newStr2.length()-1);
         System.out.println(len1);
         System.out.println(len2);
-        String[] strArr1 = new String[len1];
-        String[] strArr2 = new String[len2];
+        List<String> list1 = new ArrayList<>();
+        List<String> list2 = new ArrayList<>();
+
 
         //중복허용
         for(int i = 0; i < len1; i++){
-            strArr1[i] = newStr1.substring(i, i+2);
-            System.out.println(i+":"+strArr1[i]);
+            String str = newStr1.substring(i, i+2).replaceAll("[^a-z]","");
+            if(str.length() == 2){
+                list1.add(str);
+            }
+            System.out.println(i+":"+str);
         }
         for(int i = 0; i <len2; i++){
-            strArr2[i] = newStr2.substring(i, i+2);
-            System.out.println(i+":"+strArr2[i]);
-        }
-
-        int union = 0;
-        for(int i = 0; i <strArr1.length; i++){
-            for(int j = 0; j < strArr2.length; j++){
-                System.out.println(strArr1[i]+":"+strArr2[j]);
-                if(strArr1[i].equals(strArr2[j])){
-                    union++;
-
-                }
+            String str = newStr2.substring(i, i+2).replaceAll("[^a-z]","");
+            if(str.length() == 2){
+                list2.add(str);
             }
+            System.out.println(i+":"+str);
         }
-        System.out.println(union);
-        answer = (int)(65536*((float)union/(len1 + len2 - union)));
 
-        return answer;
+
+        List<String> intersection = new ArrayList<>();
+        List<String> union = new ArrayList<>();
+
+        for(String s : list1) {
+            if(list2.remove(s)) {
+                intersection.add(s);
+            }
+            union.add(s);
+        }
+
+        for(String s : list2) {
+            union.add(s);
+        }
+        double a = intersection.size();
+        double b = union.size();
+
+        return (int)b == 0? 65536:(int)(a/b*65536);
     }
     public static void main(String[] args){
-        String str1 = "aa1+aa2";
-        String str2 = "AAAA12";
+        String str1 = "handshake";
+        String str2 = "shake hands";
         System.out.println(solution(str1,str2));
         /*
         str1	    str2	answer
