a272577138b77ee01a30347946df133a72ab585c
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-10-14 00:44:37 +0900

    Day60 폰켓몬!! 겁나 쉽다... 빨리 Lv2를 가고 싶지만 최대한 많이 분석하기!

--- programmers/Lv1/Lv1폰켓몬.java
+++ programmers/Lv1/Lv1폰켓몬.java
@@ -1,27 +1,32 @@
 package algorithm.programmers.Lv1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Lv1폰켓몬 {
     public int solution(int[] nums) {
-        int answer = 0;
-        int len = nums.length;
-        // 중복 제거를 위한 set (순서 상관 없으므로 HashSet 사용)
+        int n = nums.length/2;
+
         Set<Integer> set = new HashSet<>();
-        // set에 모든 폰켓몬 번호를 add
-        for (int num : nums) {
+
+        for (int num:nums) {
             set.add(num);
         }
-        // set의 사이즈가 폰켓몬수/2 보다 크다면?
-        // 폰켓몬수/2 만큼의 종류를 선택할 수 있다는 의미
-        if (set.size() >= len/2) {
-            answer = len/2;
-            // set의 사이즈가 폰켓몬수/2 보다 작다면?
-            // 폰켓몬수/2 만큼의 종류를 가질 수는 없고 set의 크기만큼의 종류를 가질 수 있음
+
+        if(set.size() > n){
+            return n;
         } else {
-            answer = set.size();
+            return set.size();
         }
-        return answer;
+    }
+
+    @Test
+    public void 정답() {
+        Assertions.assertEquals(2, solution(new int[]{3, 1, 2, 3}));
+        Assertions.assertEquals(3, solution(new int[]{3,3,3,2,2,4}));
+        Assertions.assertEquals(2, solution(new int[]{3,3,3,2,2,2}));
     }
 }
