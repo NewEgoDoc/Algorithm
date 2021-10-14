dcf3c8ad5d1fcc54d47e0aaa9f18aa24e6d01d58
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-10-14 14:42:36 +0900

    Day61 124 숫자 체계문제 복습! 이웃나라 1267 나라가 있어도 해당 내용만 숙지하면 두개의 국가는 10진수로 의사소통이 가능하다. 어떻게 숫자를 관리하고 그리고 나누어 떨어지는 수에서 일치성을 보장한다면 어렵지 않게 문제를 구조화 할수 있다

--- programmers/Lv2/Lv2_124나라의숫자.java
+++ programmers/Lv2/Lv2_124나라의숫자.java
@@ -10,6 +10,8 @@ public class Lv2_124나라의숫자 {
 
     static int[] pNums = {4,1,2};
 
+    static int[] dpNums ={7,1,2,6};
+
     public String solution(int n) {
         StringBuilder sb = new StringBuilder();
         int number = n;
@@ -27,12 +29,39 @@ public class Lv2_124나라의숫자 {
         return sb.reverse().toString();
     }
 
+    public String solution2(int n) {
+        StringBuilder sb = new StringBuilder();
+        int number = n;
+        int remain;
+        while(number != 0){
+            remain = number%(dpNums.length);
+            number /= dpNums.length;
+
+            if(remain == 0) number--;
+
+            sb.append(dpNums[remain]);
+        }
+
+        return sb.reverse().toString();
+    }
+
     @Test
-    public void testAnswer() {
+    public void test124() {
         Assertions.assertEquals(solution(10),"41");
         Assertions.assertEquals(solution(1),"1");
         Assertions.assertEquals(solution(16),"121");
         Assertions.assertEquals(solution(30),"244");
         Assertions.assertEquals(solution(13),"111");
     }
+
+    @Test
+    public void test1267(){
+        Assertions.assertEquals(solution2(1),"1");
+        Assertions.assertEquals(solution2(2),"2");
+        Assertions.assertEquals(solution2(3),"6");
+        Assertions.assertEquals(solution2(4),"7");
+        Assertions.assertEquals(solution2(5),"11");
+        Assertions.assertEquals(solution2(40),"217");
+    }
+
 }
