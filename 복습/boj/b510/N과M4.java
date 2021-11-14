d308ff31de18c93b98f9de5ce3b553cac354b230
DESKTOP-M0OCTGD\jhbyeon <juhan211@naver.com>
2021-11-14 23:04:54 +0900

    Day78 가볍게 다시 한 NM4 솔직히 익숙해 져서 재미있디...

---
+++ 복습/boj/b510/N과M4.java
@@ -0,0 +1,42 @@
+package algorithm.복습.boj.b510;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class N과M4 {
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
+
+        arr = new int[M];
+        combination(0,1,N,M);
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    public static void combination(int index, int start,int N,int M){
+        if(0 == M){
+            for(int i = 0 ; i < index; i++){
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = start; i <= N; i++){
+            if(index != 0 && arr[index - 1] > i) continue;
+            arr[index] = i;
+            combination(index+1,start,N,M-1);
+        }
+    }
+}
